cd2aed887114556bc61d047965c595accb2d5c61
JefersonSatoshi <jefersonsatoshi@hotmail.com>
2026-04-26 22:53:27 -0300

    User, Role, Course

---
+++ src/main/java/com/devsuperior/dslearn/entities/Offer.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslearn.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String edition;
+	private Instant startMoment;
+	private Instant endMoment;
+	
+	public Offer() {
+		
+	}
+}
