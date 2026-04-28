e2a33ad870dcfc4df43b89ef3dce90487d2b2216
JefersonSatoshi <jefersonsatoshi@hotmail.com>
2026-04-27 22:56:21 -0300

    Offer

--- src/main/java/com/devsuperior/dslearn/entities/Course.java
+++ src/main/java/com/devsuperior/dslearn/entities/Course.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dslearn.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,6 +25,9 @@ private static final long serialVersionUID = 1L;
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
+	
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
 	
 	public Course() {
 
@@ -65,6 +71,10 @@ private static final long serialVersionUID = 1L;
 
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
+	}
+
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,3 +13,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Estudo de casos', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMxEB2IcNfkOhtWPzbMRI2a1xgFaWfR4f1Wg&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkiENcfzewm0pudNUc6Dza8a2sXr3YXzrAMQ&s');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1)
